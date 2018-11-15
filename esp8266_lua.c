a513ea329c86ce6f4a52d7bbc88bbbf79911c973
David Pirogovsky <dap263@cornell.edu>
2018-11-15 18:06:14 -0500

    stuff

--- esp8266_lua.c
+++ esp8266_lua.c
@@ -1,7 +1,13 @@
+// will probably have to escape all the quotes with \
+
+
 printf("srv=net.createConnection(net.TCP,0)")
 printf("srv:on("receive",function(sck,c) print(c) end)")
-printf("srv:on("connection",function(sck,c))
+printf("srv:on("connection",function(sck,c)")
 // change this to index.php probably 
-printf("sck:send("GET /index.html HTTP /1.1\r\nHost: http://127.0.0.1\r\nConnection: close\r\nAccept: */*\r\n\r\n")
+// just copy for PuTTY
+printf("sck:send("GET 0.0045, 56.21 HTTP /1.1\r\nHost: 192.168.4.2\r\nConnection: close\r\nAccept: */*\r\n\r\n")")
+// what to actually use
+//printf("sck:send(\"GET 0.0045, 56.21 HTTP /1.1\r\nHost: 192.168.4.2\r\nConnection: close\r\nAccept: */*\r\n\r\n\")")
 printf("end)")
-printf("srv:connect(80,"192.168.4.2")")
+printf("srv:connect(5000,"192.168.4.2")")
