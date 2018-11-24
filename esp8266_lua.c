7d9c6c25e1cb8ce7c370b74a45c67b36153bd350
David Pirogovsky <dap263@cornell.edu>
2018-11-24 13:04:35 -0500

    server can detect different request types, network receiving still isn't right

--- esp8266_lua.c
+++ esp8266_lua.c
@@ -1,13 +1,24 @@
 // will probably have to escape all the quotes with \
 
+// connect to network
+wifi.setmode(wifi.STATION)
+delay(1000)
+station_cfg = {}
+station_cfg.ssid="FBI"
+station_cfg.pwd="verystrong"
+station_cfg.save=true
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
+print(wifi.sta.getip())
 
 printf("srv=net.createConnection(net.TCP,0)")
 printf("srv:on("receive",function(sck,c) print(c) end)")
 printf("srv:on("connection",function(sck,c)")
-// change this to index.php probably 
-// just copy for PuTTY
-printf("sck:send("GET 0.0045, 56.21 HTTP /1.1\r\nHost: 192.168.4.2\r\nConnection: close\r\nAccept: */*\r\n\r\n")")
-// what to actually use
+// 192.168.4.2 in softap mode
+// ra, dec: x.xxxxx, x.xxxxx
+// tts: 'words to speak'
+printf("sck:send("GET tts: hello HTTP /1.1\r\nHost: 192.168.43.1\r\nConnection: close\r\nAccept: */*\r\n\r\n")")
+// what to use in softap
 //printf("sck:send(\"GET 0.0045, 56.21 HTTP /1.1\r\nHost: 192.168.4.2\r\nConnection: close\r\nAccept: */*\r\n\r\n\")")
 printf("end)")
-printf("srv:connect(5000,"192.168.4.2")")
+printf("srv:connect(5000,"192.168.43.14")")
--- server.py
+++ server.py
@@ -1,15 +1,59 @@
 import socket
+import os
+path = r"G:\My Drive\Classes\4760\Final Project\hygdata_v3.csv"
+path_speech = r"G:\My Drive\Classes\4760\Final Project\andromeda.wav"
+cwd = os.getcwd()
+path_speech = cwd + "\andromeda.wav"
+
+# setup csv
+import csv_parser
+csv_parser.setup_csv()
+print('csv parsed')
+
+# setup TTS
+from comtypes.client import CreateObject
+import win32com.client
+from comtypes.gen import SpeechLib
+
+def do_TTS(msg):
+    print('starting tts')
+    engine = CreateObject("SAPI.SpVoice")
+    stream = CreateObject("SAPI.SpFileStream")
+    stream.Open('temp.wav', SpeechLib.SSFMCreateForWrite)
+    engine.AudioOutputStream = stream
+    # need to parse request to find requested message
+    ind = msg.find(': ')
+    ind1 = msg.find('\r\n')
+    word = msg[ind+1:ind1]
+    engine.speak(word, 0)
+    stream.Close()
+    # need to change this to temp if i want it to be overwritten each time
+    f = open("temp.wav", 'rb')
+    l = f.read(44) # strip off the wav header
+    l = f.read(1024)
+    l = l + '\r\n\r\n'
+    while (l):
+        print 'sending'
+        cltskt.send(l)
+        l = f.read(1024)
+    f.close()
+    print 'done sending'
 
 # base code taken from CS 4410 lecture with Robbert Van Renesse
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 skt.bind(('',5000))
 skt.listen(5)
-
+ra = 0
+dec = 0
 while True:
     cltskt, addr = skt.accept()
-    msg = cltskt.recv(100)
+    msg = cltskt.recv(1000)
     print("got '%s' from %s" % (msg, addr))
-
+    if (msg.find('ra') != -1):
+        # need to change ra and dec to specific positions in get request
+        csv_parser.search_csv(ra, dec)
+    elif (msg.find('tts') != -1):
+        do_TTS(msg)
     body = "<html><body>Hello {}</body></html>".format(addr)
     response = "HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n" % len(body)
     cltskt.send(response + body)
