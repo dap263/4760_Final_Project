ea3da02ddea89cb9e2065bc65e4696325b4f1aa2
Alec Newport <acn55@cornell.edu>
2018-12-09 16:11:17 -0500

    Fixed GPS.h comments

--- WiFi_GPS_Project/GPS.h
+++ WiFi_GPS_Project/GPS.h
@@ -1,8 +1,11 @@
 /* 
  * File:   GPS.h
- * Author: Alec
+ * Author: Alec Newport (acn55)
  *
  * Created on November 16, 2018, 12:42 PM
+ * Modified from ECE 4760 Project Borkbit (file GPS_UART.h)
+ * Authors: Vaidehi Garg, Vidya Ramesh, and Divya Gupta
+ * http://people.ece.cornell.edu/land/courses/ece4760/FinalProjects/f2017/vg254_dg524_vr236/vg254_dg524_vr236/vg254_dg524_vr236/vg254_dg524_vr236.html
  */
 
 #ifndef GPS_H
@@ -12,24 +15,11 @@
 extern "C" {
 #endif
 
-/* Authors: Junyin Chen (jc2954@cornell.edu)
- *          Haonan Liu  (hl955@cornell.edu)
- * 
- * Borrowed & modified from the following ECE4760 project: https://goo.gl/ccsXBv
- * 
- */
-
-//====================================================================
-// build a string from the UART2 /////////////
-//////////////////////////////////////////////
 #define max_chars_GPS 256   // for input/output buffer
 char PT_term_buffer_GPS[max_chars_GPS];	//buffer for storing string from UART2
 char PT_term_buffer_GPS_RMC[max_chars_GPS];	//buffer for storing GPRMC string
-int num_char_GPS;					//number of characters in the string of UART2
-//int num_send_chars;
-//unsigned char GPGGA; // Boolean GPS string is GGA format
+int num_char_GPS;					//number of characters in the string of UART1
 unsigned char GPRMC; // Boolean GPS string is RMC format
-//unsigned char OKrn;
 char *RMCptr;
 char *RMCend;
 
@@ -85,55 +75,6 @@ int PT_GetSerialBufferGPS(struct pt *pt)
     PT_END(pt);
 }
 
-//====================================================================
-// build a string from the UART2 /////////////
-//////////////////////////////////////////////
-//#define max_chars_WiFi 256
-//char PT_term_buffer_WiFi[max_chars_GPS]; //buffer for storing string from UART1
-//int num_char_WiFi; // number of characters in the string from UART1
-//char PT_terminate_char_WiFi; // WiFi UART termination character
-//
-//int PT_GetSerialBufferWiFi(struct pt *pt)
-//{
-//    
-//    static char character;
-//    
-//    // mark the beginning of the input thread
-//    PT_BEGIN(pt);
-//
-//    num_char_WiFi = 0;
-//
-//    while(num_char_WiFi < max_chars_WiFi)
-//    {	//if frame work happens, just throw that away
-//        if(UART1GetErrors() & 0x02){
-//            UART1ClearAllErrors();
-//            PT_YIELD_TIME_msec(1);
-//            break;
-//        }
-//        // get the character
-//        // yield until there is a valid character so that other threads can
-//        // execute
-//        PT_YIELD_UNTIL(pt, UARTReceivedDataIsAvailable(UART1));
-//        
-//        character = UARTGetDataByte(UART1);
-//        
-//        if ((character>0) && (character == PT_terminate_char_WiFi)) {
-//            // leave the while loop
-//            break;
-//        }
-//        
-//        PT_term_buffer_WiFi[num_char_WiFi++] = character;
-//        
-//    } //end while(num_char < max_size)
-//    
-//    PT_term_buffer_WiFi[num_char_WiFi] = 0; // zero terminate the string
-//
-//    // kill this input thread, to allow spawning thread to execute
-//    PT_EXIT(pt);
-//    // and indicate the end of the thread
-//    PT_END(pt);
-//}
-
 float GPS_Lat;	// Latitude in degrees (positive = North, negative = South)
 float GPS_Lon;	// Longitude in degrees (positive = East, negative = West)
 char GPS_type[6];	
@@ -186,35 +127,8 @@ void parse_RMC(char* PT_term_buffer_GPS) {
     GPS_year = GPS_date - (GPS_day*10000) - (GPS_month*100) + 2000;
 }
 
-// Parse a $GPGGA NMEA string
-//void parse_GGA(char* PT_term_buffer_GPS){
-//    // Temp variables
-//    float GPS_time, GPS_dmLat, GPS_dmLon;
-//    char GPS_NS, GPS_WE;
-//    int GPS_Lat_deg, GPS_Lon_deg;
-//    float GPS_Lat_frac, GPS_Lon_frac;
-//    
-//    sscanf(PT_term_buffer_GPS, "%6c,%f,%f,%c,%f,%c,%d", 
-//            GPS_type, &GPS_time, &GPS_dmLat, &GPS_NS, &GPS_dmLon,
-//            &GPS_WE, &GPS_quality);
-//    
-//    // Convert from ddmm.m to d.d format for latitude and longitude
-//    GPS_Lat_deg = GPS_dmLat / 100;
-//    GPS_Lon_deg = GPS_dmLon / 100;
-//    GPS_Lat_frac = GPS_dmLat - (float) GPS_Lat_deg;
-//    GPS_Lon_frac = GPS_dmLat - (float) GPS_Lon_deg;
-//    GPS_Lat = (float) GPS_Lat_deg + GPS_Lat_frac/60; // Return value
-//    GPS_Lon = (float) GPS_Lon_deg + GPS_Lon_frac/60; // Return value
-//    
-//    GPS_time_h = GPS_time / 10000;
-//    GPS_time_m = (GPS_time - GPS_time_h) / 100;
-//    GPS_time_s = GPS_time - GPS_time_h - GPS_time_m;
-//}
-
-
 #ifdef	__cplusplus
 }
 #endif
 
 #endif	/* GPS_H */
-
